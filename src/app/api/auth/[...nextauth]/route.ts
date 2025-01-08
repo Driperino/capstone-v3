17bac85800455c9dc88e00bcb30806ea0b1b1c1d
Vincent <vincent.winkler98@gmail.com>
2025-01-08 01:56:19 -0800

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth';
 import { authOptions } from '@/lib/authOptions';
 
+//all of a sudden this is causing the build to fail????
+
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
